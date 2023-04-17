03986a21c69fb98bac27fc13cf0ad795e5f8d8a0
oss0430 <oss002@ajou.ac.kr>
2023-04-17 08:57:21 +0000

    Added word2vec method

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from modules.dataset import DCDataset
-from modules.algorithms import KnnForDC, NaieveBayesForDC
+from modules.algorithms import KnnForDC, NaieveBayesForDC, Word2VecForDC
+from modules.evaluation import evaluate_confusion_matrix
 import nltk
+import json
 #nltk.download('punkt')
+
+def _result_to_file_json_file(
+    path : str,
+    result : dict
+) :
+    evaluation_result = evaluate_confusion_matrix(result["confusion_matrix"])
+    evaluation_result["acc"] = result["acc"]
+
+    with open(path, "w") as f:
+        json.dump(evaluation_result, f)
+
+    
 
 def main():
     ## get dataset
     dataset = DCDataset(path = "dataset/News_Category_Dataset_v3_balanced.json")
     dataset_for_scikit = dataset.get_dataset_for_scikitlearn()
     
-    #print(dataset.df.head(5))
-    #print(dataset_for_scikit["X"][0], dataset_for_scikit["Y"][0])
-    #print(dataset.categories)
-    
     train_dataset, test_dataset = dataset.split_train_test()
     
     #algorithms = [KnnForDC(dataset.categories)]
-    algorithms = [NaieveBayesForDC(dataset.categories, n_gram = 3)]
+    #algorithms = [NaieveBayesForDC(dataset.categories, n_gram = 3)]
+    algorithms = [Word2VecForDC(dataset.categories)]
+    #algorithms = [KnnForDc(dataset.categories),
+    #              NaieveBayesForDC(dataset.categories, n_gram = 1), 
+    #              NaieveBayesForDC(dataset.categories, n_gram = 2),
+    #              NaieveBayesForDC(dataset.categories, n_gram = 3),
+    #              Word2VecForDC(dataset.categories)]
     
     for algorithm in algorithms:
         algorithm.train(train_dataset, method = "concat")
